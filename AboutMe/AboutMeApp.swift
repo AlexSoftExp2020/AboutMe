7d1be5f5fa31dc39472a5332e67d9025a91aeac8
Alex <oleksii.pidhornyi@gmail.com>
2024-12-15 14:31:54 +0100

    Initial Commit

---
+++ AboutMe/AboutMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AboutMeApp.swift
+//  AboutMe
+//
+//  Created by Oleksii on 15.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct AboutMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

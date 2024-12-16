464d8d39652f5ddf522d0952bdcbb995a4b8fead
Alex <oleksii.pidhornyi@gmail.com>
2024-12-16 20:56:52 +0100

    add fanfacts tab

--- AboutMe/Views/FanFactsView.swift
+++ AboutMe/Views/FanFactsView.swift
@@ -8,8 +8,23 @@
 import SwiftUI
 
 struct FanFactsView: View {
+    @State private var funFact = ""
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("Fun Facts")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+            
+            Text(funFact)
+                .padding()
+                .font(.title)
+                .frame(minHeight: 400)
+            
+            Button("Show Random Fact") {
+                funFact = information.funfacts.randomElement()!
+            }
+        }
+        .padding()
     }
 }
 
